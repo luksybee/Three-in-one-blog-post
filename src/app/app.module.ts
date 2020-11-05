dd368d14818c8056bfa6f0577e392a1ae568ad6e
luksyb1 <57903572+luksyb1@users.noreply.github.com>
2020-11-05 14:24:02 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,8 @@ import { HobbyModule } from './hobby/hobby.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/three-in-one-db'),
+    // MongooseModule.forRoot('mongodb://localhost:27017/three-in-one-db'),
+    MongooseModule.forRoot('mongodb+srv://abefe:1111111111@cluster0.9ezx3.mongodb.net/three-in-one?retryWrites=true&w=majority'),
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       sortSchema: true,
